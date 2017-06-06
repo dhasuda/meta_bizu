fde3d8afdc3ce595bb6ffe24c831c8266696d78c
dhasuda <davihasuda@hotmail.com>
2017-06-06 06:43:38 -0300

    identation correction

--- assessment/models.py
+++ assessment/models.py
@@ -3,9 +3,9 @@ from django.db import models
 """ The model names are bad... think about something better """
 
 #class Objeto(models.Model):
-    """ It has a name (i.e.: Yano) and a type (i.e.: teacher) """
+""" It has a name (i.e.: Yano) and a type (i.e.: teacher) """
 
 #class Avaliacao(models.Model):
-    """ It has a rank and some text and other stuff """
-    """ precisa estar relacionado com um objeto """
-    #objeto = models.ForeignKey(Objeto)
+""" It has a rank and some text and other stuff """
+""" precisa estar relacionado com um objeto """
+#objeto = models.ForeignKey(Objeto)
