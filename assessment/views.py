485934c54a5dfe32db268794a9810e84a984a7e3
dhasuda <davihasuda@hotmail.com>
2017-06-06 20:05:27 -0300

    getting forms information about review

--- assessment/views.py
+++ assessment/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from assessment.models import Item, Opinion
 
 def home(request):
     return render(request, 'assessment/test.html',)
 
 def search(request):
     return render(request, 'assessment/test.html',)
+
+def review(request):
+    if request.method == 'POST':
+        form = Opinion(request.POST)
+
+        if form.is_valid():
+            # Save the new Opinion to the database
+            form.save(commit=True)
+            return home(request)
+
+        else:
+            print (form.erros)
+
+    else:
+        form = Opinion()
+    return render(request, 'assessment/test.html', {'form': form})
