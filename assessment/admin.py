bc1fa55a0550182c6c680ebdba6af88996868886
dhasuda <davihasuda@hotmail.com>
2017-06-07 04:00:39 -0300

    starting users authentication

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from assessment.models import Item, Opinion
+from assessment.models import Item, Opinion, UserProfile
 
 admin.site.register(Item)
 
 admin.site.register(Opinion)
+
+admin.site.register(UserProfile)
