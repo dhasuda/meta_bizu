bd49136ffca0df282e57ce75cc301cabf2544325
dhasuda <davihasuda@hotmail.com>
2017-06-07 06:23:58 -0300

    translation

--- assessment/forms.py
+++ assessment/forms.py
@@ -5,30 +5,30 @@ from django.contrib.auth.models import User
 
 ##ADD ITEM
 class ItemForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Name")
+    nome = forms.CharField(max_length=128, help_text="nome")
     category = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     # An inline class to provide additional information on the forms
     class Meta:
         model = Item
-        fields = ('name',)
+        fields = ('nome',)
 
 #SEARCH ITEM
 class ItemSearchForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Name")
+    nome = forms.CharField(max_length=128, help_text="nome")
 
     # An inline class to provide additional information on the forms
     class Meta:
         model = SearchText
-        fields = ('name',)
+        fields = ('nome',)
 
 class OpinionForm(forms.ModelForm):
     rank = forms.IntegerField(initial=5)
-    description = forms.CharField(max_length=500, help_text="Comment about it here")
+    comentario = forms.CharField(max_length=500, help_text="Comment about it here")
 
     class Meta:
         model = Opinion
-        fields = ('rank', 'description',)
+        fields = ('rank', 'comentario',)
         #exclude = ('Item',)
 
 class UserForm(forms.ModelForm):
